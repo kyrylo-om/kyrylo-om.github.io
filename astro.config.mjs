585cb8d5e9046a3a50f60cd478e59b51b553a737
Kyrylo Omelianchuk <kyrylo.omelianchuk@gmail.com>
2026-08-08 10:22:14 +0300

    Rough layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kyrylo-om.github.io/',
@@ -12,6 +10,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
 });
